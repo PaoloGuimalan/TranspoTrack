a31d539b9c114731e22d80b8ab0e432fc3c82d3f
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-05-13 19:41:01 -0900

    link change

--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,5 @@
 export const URL_ONE = 'localhost:3001'
-export const URL_TWO = 'adventurous-elk-getup.cyclic.app'
+export const URL_TWO = 'transpotrackserver.herokuapp.com'
 
 //'adventurous-elk-getup.cyclic.app'
+//https://transpotrackserver.herokuapp.com/
