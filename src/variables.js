fa4c95598f1f05d061887cddb66580c2091d287e
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-02-09 20:33:16 -0900

    Setup link for railway

--- src/variables.js
+++ src/variables.js
@@ -1,5 +1,6 @@
 export const URL_ONE = 'localhost:3001'
-export const URL_TWO = 'transpotrackserver.herokuapp.com'
+// export const URL_TWO = 'transpotrackserver.herokuapp.com'
+export const URL_TWO = 'transpotrackserver-production.up.railway.app';
 
 //'adventurous-elk-getup.cyclic.app'
 //https://transpotrackserver.herokuapp.com/
