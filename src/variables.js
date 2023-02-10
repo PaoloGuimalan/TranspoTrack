1938dfdfd726f6474bbcb798874e8ba1be158cb9
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-02-10 06:37:50 -0900

    variable link modified for production

--- src/variables.js
+++ src/variables.js
@@ -1,6 +1,6 @@
-export const URL_TWO = 'http://localhost:3001'
+// export const URL_TWO = 'http://localhost:3001'
 // export const URL_TWO = 'https://transpotrackserver.herokuapp.com'
-// export const URL_TWO = 'https://transpotrackserver-production.up.railway.app';
+export const URL_TWO = 'https://transpotrackserver-production.up.railway.app';
 
 //'adventurous-elk-getup.cyclic.app'
 //https://transpotrackserver.herokuapp.com/
