2dcf1bc474c627235a4854f2ae775cb42530cde4
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-03-14 15:45:13 -0900

    Info toggle state is now passed along with location sharing details for active drivers to ensure of preventing active driver disconnection

--- src/variables.js
+++ src/variables.js
@@ -1,7 +1,7 @@
-export const URL_TWO = 'http://localhost:3001'
+// export const URL_TWO = 'http://localhost:3001'
 // export const URL_TWO = 'https://transpotrackserver.herokuapp.com'
 // export const URL_TWO = 'https://transpotrackserver-production.up.railway.app';
-// export const URL_TWO = 'https://transpotrackserver.onrender.com'
+export const URL_TWO = 'https://transpotrackserver.onrender.com'
 
 //'adventurous-elk-getup.cyclic.app'
 //https://transpotrackserver.herokuapp.com/
